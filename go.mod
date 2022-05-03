184d024a8274e4510e9c34d3209ae6171c4462f5
sudachi0114 <jnevariste@gmail.com>
2022-05-03 19:39:15 +0900

    リクエストのログを記録する middleware を定義

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sudachi0114/gin-rest-backend-trial
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.7.7
+require (
+	github.com/gin-gonic/gin v1.7.7
+	go.uber.org/zap v1.21.0
+)
--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sudachi0114/gin-rest-backend-trial/middleware"
 )
 
 func main() {
 	engine := gin.Default()
+
+	engine.Use(middleware.RecordUaAndTime)
 
 	engine.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
